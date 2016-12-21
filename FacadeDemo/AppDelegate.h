114b5eb013597fd2ee1fa16632a8187b9c58b79d
Nikhil Chawade <nikhilchawade@nikhils-mac-mini.local>
2016-12-21 11:10:12 +0530

    Initial Commit

---
+++ FacadeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FacadeDemo
+//
+//  Created by Nikhil Chawade on 21/12/16.
+//  Copyright © 2016 TheLeanApps Solution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FacadeDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  FacadeDemo
+//
+//  Created by Nikhil Chawade on 21/12/16.
+//  Copyright © 2016 TheLeanApps Solution. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
